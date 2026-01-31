06c2505be422995ca8152b4dd0038d853f2ebd68
Andranik Arakelyan <andranik.arakelyan.me@gmail.com>
2026-01-31 11:08:11 +0400

    initial commit

---
+++ apps/web/electron/main.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+        },
+    });
+
+    if(isDev) {
+        win.loadURL('http://localhost:5007');
+    }
+    else {
+        win.loadFile(path.join(__dirname, '../out/index.html'));
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
